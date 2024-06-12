cb6234a36162ab00646611bf12f72bf6f60eadc9
damilhome <damilhome@gmail.com>
2024-06-12 16:50:37 -0300

    sistema completo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar classe scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        //Exibir as mensagens para o usuário e obter os valores digitados no terminal através do scanner
+        System.out.println("Digite o número da conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite seu nome completo: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
+        scanner.nextLine();
+
+        //Exibir mensagem de conta criada
+        String mensagemFormatada = String.format("Olá %s, obrigado por criar uma conta em nosso banco. Os dados de sua conta são: agência %s, conta %d. Seu saldo de %.2f já está disponível para saque.", nome, agencia, numero, saldo);
+        System.out.println(mensagemFormatada);
+
+        scanner.close();
     }
 }
